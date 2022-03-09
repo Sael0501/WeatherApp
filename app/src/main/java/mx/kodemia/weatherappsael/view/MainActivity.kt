9ac12e2c9dc92c6d384a0a2d44f2896f0b6fdff3
Sael0501 <saelaceves117@gmail.com>
2022-03-09 01:09:57 -0600

    Creacion de paquetes

--- app/src/main/java/mx/kodemia/weatherappsael/view/MainActivity.kt
+++ app/src/main/java/mx/kodemia/weatherappsael/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package mx.kodemia.weatherappsael
+package mx.kodemia.weatherappsael.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import mx.kodemia.weatherappsael.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
