aa1921f7405baf2853c5425dd10219a37c7809a6
Sael0501 <saelaceves117@gmail.com>
2022-03-09 23:27:04 -0600

    CardView del clima en horas

--- app/src/main/java/mx/kodemia/weatherappsael/utils/Utilis.kt
+++ app/src/main/java/mx/kodemia/weatherappsael/utils/Utilis.kt
@@ -8,16 +8,21 @@ import android.os.Build
 fun checkForInternet(context: Context): Boolean{
     //Registrar la actividad con el servicio connectivity manager
     val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
     // Si la version de Android es M o mayor se usa NetworkCapabilities
     if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+
         //Devuelve un objeto de tipo Network correspondiente a la conectividad del dispositivo
         val network = connectivityManager.activeNetwork ?: return false
+
         //Representation of the capabilities of an active network
         val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
 
         return when {
+
             //Indica si la red usa transporte WiFi o tiene conectividad WiFi
             activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+
             //Indica si la red tiene conectividad por datos moviles
             activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
 
